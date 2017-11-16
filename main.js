c8699bb1e2b06a40b42940d9feadd8adf1041b22
Lisa Lehrman <lisa.lehrman@gmail.com>
2017-11-15 17:55:28 -0800

    adds tween animation for sprite

--- main.js
+++ main.js
@@ -15,14 +15,12 @@ var GameState = {
 		this.load.image('burger', 'assets/imgs/burger.png');
 		this.load.image('toy', 'assets/imgs/toy.png');
 		this.load.image('rotate', 'assets/imgs/rotate.png');
-
-
-
 		this.load.spritesheet('pet', 'assets/imgs/virtual-pet.png', 32, 32, 14);
 	},
 	//executed last
 	create: function() {
 		this.background = this.game.add.sprite(0, 0, 'background');
+
 		this.pet = this.game.add.sprite(175, 300, 'pet');
 		this.pet.anchor.setTo(0.5);
 		this.pet.scale.setTo(3.5);
@@ -34,15 +32,81 @@ var GameState = {
 		this.pet.inputEnabled = true;
 		this.pet.input.enableDrag();
 
-		//creates sprites on canvas
+		//creates sprites on canvas, sets size and anchor point and allows user input
 		this.strawberry = this.game.add.sprite(55, 570, 'strawberry');
+		this.strawberry.anchor.setTo(0.5);
 		this.strawberry.scale.setTo(1.2);
+		this.strawberry.inputEnabled = true;
+		this.strawberry.events.onInputDown.add(this.pickItem, this);
+		this.strawberry.customParams = {health: 20};
+
 		this.burger = this.game.add.sprite(130, 570, 'burger');
+		this.burger.anchor.setTo(0.5);
+		this.burger.inputEnabled = true;
+		this.burger.events.onInputDown.add(this.pickItem, this);
+		this.burger.customParams = {health: -20, fun: 10};
+
+
 		this.toy = this.game.add.sprite(216, 570, 'toy');
+		this.toy.anchor.setTo(0.5);
+		this.toy.inputEnabled = true;
+		this.toy.events.onInputDown.add(this.pickItem, this);
+		this.toy.customParams = {fun: 10};
+
+
 		this.rotate = this.game.add.sprite(288, 570, 'rotate');
 		this.rotate.scale.setTo(.6);
+		this.rotate.anchor.setTo(0.5);
+		this.rotate.inputEnabled = true;
+		this.rotate.events.onInputDown.add(this.rotatePet, this);
 
+		this.buttons = [this.strawberry, this.burger, this.toy, this.rotate];
+
+		//nothing is selected
+		this.selectedItem = null;
+		//checks if ui is blocked 
+		this.uiBlocked = false;
 	},
+
+	//pick item method 
+	pickItem: function(sprite, event){
+		if(!this.uiBlocked) {
+		console.log('pick item');
+		this.clearSelection();
+		//alpha property makes sprite semi transparent
+		sprite.alpha = 0.5;
+		this.selectedItem = sprite;
+		}
+	},
+	//rotate pet method
+	rotatePet: function(sprite, event){
+		if(!this.uiBlocked) {
+		console.log('rotating');
+		this.uiBlocked = true;
+
+		this.clearSelection();
+		sprite.alpha = 0.5;
+// tween animation rotates pet 
+		var petRotation = this.game.add.tween(this.pet);
+		petRotation.to({angle: '+720'}, 1000);
+		petRotation.onComplete.add(function(){
+			this.uiBlocked = false;
+			sprite.alpha = 1;
+			this.pet.customParams.fun += 10;
+			console.log(this.pet.customParams.fun);
+		}, this);
+		petRotation.start();
+		}
+	},
+
+	clearSelection: function() {
+		//loops through buttons array and changes transparency(alpha) back to 1
+		//used inside of pickItem method
+		this.buttons.forEach(function(element, index){
+			element.alpha = 1;
+		});
+		this.selectedItem = null;
+	}
 };
 
 //initiate the Phaser framework
